083042a49f5a91f68bfa1da12502678577a546b8
jonas17-11 <jonas.sperlin@gmail.com>
2025-10-24 13:53:01 +0200

    Update prognose_to_discord.py

--- prognose_to_discord.py
+++ prognose_to_discord.py
@@ -1,9 +1,11 @@
 import requests
-import time
 from analyzer import run_analysis
 import os
 
-WEBHOOK_URL = os.getenv("WEBHOOK_URL") or "DEIN_DISCORD_WEBHOOK_HIER"
+WEBHOOK_URL = os.getenv("PROGNOSE_WEBHOOK")
+
+if not WEBHOOK_URL:
+    raise ValueError("❌ PROGNOSE_WEBHOOK Secret nicht gefunden. Bitte in GitHub Secrets setzen.")
 
 def post_to_discord():
     message = run_analysis()
@@ -15,8 +17,5 @@ def post_to_discord():
         print("✅ Prognose erfolgreich an Discord gesendet.")
 
 if __name__ == "__main__":
-    while True:
-        print("📤 Sende Marktprognose an Discord...")
-        post_to_discord()
-        print("🕒 Warte 1 Stunde...\n")
-        time.sleep(3600)
+    print("📤 Sende Marktprognose an Discord...")
+    post_to_discord()
